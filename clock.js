6568349c5552e67e75b153c38adb08a6606f0c3f
Enes Alili <enesalili00@gmail.com>
2020-06-06 23:36:01 +0200

    Add comments to clock

--- clock.js
+++ clock.js
@@ -2,21 +2,27 @@ var hoursContainer = document.querySelector(".hours");
 var minutesContainer = document.querySelector(".minutes");
 var tickElements = Array.from(document.querySelectorAll(".tick"));
 
+// Create a Date object and setting it to UTC (Coordinated Universal Time) zone
 var last = new Date(0);
 last.setUTCHours(-1);
 
+// Function to update the time
 function updateTime() {
   var now = new Date();
 
+  // Getting hours
   var lastHours = last.getHours().toString();
   var nowHours = now.getHours().toString();
   if (lastHours !== nowHours) {
+    // update html div of hours
     updateContainer(hoursContainer, nowHours);
   }
 
+  // Gettings minutes
   var lastMinutes = last.getMinutes().toString();
   var nowMinutes = now.getMinutes().toString();
   if (lastMinutes !== nowMinutes) {
+    // updating html div of minutes
     updateContainer(minutesContainer, nowMinutes);
   }
 
